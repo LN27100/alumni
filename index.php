331cb1b16dfd542a9db15a0d4ec3a2b43e5bdcb8
LN27100 <heloceinlove@hotmail.fr>
2023-12-20 08:44:36 +0100

    effet parallax css

--- index.php
+++ index.php
@@ -11,18 +11,24 @@ include './assets/data/data.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="./assets/style/style.css">
 <title>ALUMNI AFPA</title>
 </head>
 
 <body>
-
 <header>
     <?php include '../alumni/components/navbar.php'; ?>
 </header>
 
 
-
+<div>Bienvenue sur le site des anciens élèves de l'Afpa. Inscrivez-vous et retrouvez vos anciens camarades de sessions pour savoir ce qu'ils sont devenus, partager votre parcours, vos expériences.</div>
+<div class="parallax parallax1">
+  <h1>ALUMNI AFPA</h1>
+</div>
+<div>Contactez vos anciens camarades et organisez des retrouvailles ! Les réunions d'anciens élèves sont affichées dans l'agenda de la communauté alors n'hésitez pas à vous inscrire à l'une d'entre elles pour y revoir vos collègues de formations mais aussi vos formateurs !  </div>
+<div class="parallax parallax2">
+  <h1>ALUMNI AFPA</h1>
+</div>
 
 <footer>
 <?php include '../alumni/components/footer.php'; ?>
