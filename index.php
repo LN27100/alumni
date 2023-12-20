cb96a0042a5d8338d5b16edf62ce0d71848de7bc
LN27100 <heloceinlove@hotmail.fr>
2023-12-20 13:08:03 +0100

    essai image

--- index.php
+++ index.php
@@ -1,60 +1,61 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<link rel="stylesheet" href="./assets/style/style.css">
-<title>ALUMNI AFPA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style/style.css">
+    <title>ALUMNI AFPA</title>
 </head>
 
 <body>
-<header>
-    <?php include '../alumni/components/navbar.php'; ?>
-</header>
+    <header>
+        <?php include '../alumni/components/navbar.php'; ?>
+    </header>
 
 
-<div>Bienvenue sur le site des anciens élèves de l'Afpa. Inscrivez-vous et retrouvez vos anciens camarades de sessions pour savoir ce qu'ils sont devenus, partager votre parcours, vos expériences.</div>
-<div class="parallax parallax1">
-  <h1>ALUMNI AFPA</h1>
+    <div>Bienvenue sur le site des anciens élèves de l'Afpa. Inscrivez-vous et retrouvez vos anciens camarades de sessions pour savoir ce qu'ils sont devenus, partager votre parcours, vos expériences.</div>
+    <div class="parallax parallax1">
+        <h1>ALUMNI AFPA<img src="/assets/img/AFPA-symbole.jpg" class="symbole" alt="symbole afpa"></h1>
 
 
-  <div class="container">
+        <div class="container">
             <div class="row">
                 <?php
                 include './assets/data/data.php';
                 foreach ($data as $item) {
-                    echo '<div class="col-md-3">';
+                    echo '<div class="child">';
                     echo '<div class="card">';
                     echo '<div class="card-body">';
-                    echo '<h5 class="card-title">'. $item['name'] . '</h5>';
+                    echo '<h5 class="card-title">' . $item['name'] . '</h5>';
+                    echo '<img class= "img" src="./assets/img/portrait-1.jpg" /> ';
                     echo '<p class="card-text">' . $item['region'] . '</p>';
-                    echo '<p class="card-text">' . $item['phone'] . '</p>';
-                    echo '<p class="card-text">' . $item['email'] . '</p>';
                     echo '</div></div></div>';
                 }
                 ?>
 
             </div>
         </div>
- 
 
 
 
 
-</div>
-<div class="transit">Contactez vos anciens camarades et organisez des retrouvailles ! Les réunions d'anciens élèves sont affichées dans l'agenda de la communauté alors n'hésitez pas à vous inscrire à l'une d'entre elles pour y revoir vos collègues de formations mais aussi vos formateurs !  </div>
-<div class="parallax parallax2">
-  <h1>ALUMNI AFPA</h1>
-  
+
+    </div>
+    <div class="transit">Contactez vos anciens camarades et organisez des retrouvailles ! Les réunions d'anciens élèves sont affichées dans l'agenda de la communauté alors n'hésitez pas à vous inscrire à l'une d'entre elles pour y revoir vos collègues de formations mais aussi vos formateurs ! </div>
+    <div class="parallax parallax2">
+        <h1>ALUMNI AFPA</h1>
 
 
-</div>
 
-<footer>
-<?php include '../alumni/components/footer.php'; ?>
-</footer>
+    </div>
+
+    <footer>
+        <?php include '../alumni/components/footer.php'; ?>
+    </footer>
 
 
 </body>
+
 </html>
